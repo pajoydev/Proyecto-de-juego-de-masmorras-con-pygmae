c21f8d0be02b9e9fd46b480292710c8e3ebb2017
pajoydev <pajoydev@gmail.com>
2025-12-29 16:21:33 -0500

    fix(index.py): se a optimizado el codigo de el tilemap

--- Juego/index.py
+++ Juego/index.py
@@ -11,27 +11,50 @@ def process():
     player.update(walls)
 
 def dibujar(window):
+    #colsiones de paredes
     for static in walls:
         static.reset(window)
+    
+    #solo piso no colisiona
+    for static in floor:
+        static.reset(window)
+    
     player.reset(window)
 
-def make_map(tile_map):
+# --- create maps ---
+def make_map_colision(tile_map):
     walls = sprite.Group()
 
     for y, fila in enumerate(tile_map):
         for x, col in enumerate(fila):
             if col == '1':
-                pared = ObjetStatic(
+                walls.add(
+                    ObjetStatic(
                     x * TILE_SIZE,
                     y * TILE_SIZE,
-                    'TileMap/tile_0000.png'
-                )
-                walls.add(pared)
-
+                    TILE_WALL_IMG
+                ))
+                
     return walls
 
+def make_map_layers(tile_map):
+    floor = sprite.Group()
+
+    for y, fila in enumerate(tile_map):
+        for x, col in enumerate(fila):
+            if col == '0':
+                floor.add(
+                    ObjetStatic(
+                    x * TILE_SIZE,
+                    y * TILE_SIZE,
+                    TILE_FLOOR_IMG
+                ))
+                
+    return floor
+
+
 # --- Configuración ---
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 640, 384
 FPS = 60
 BG_COLOR = (20, 20, 20)
 TITLE = "Pantalla base"
@@ -42,9 +65,27 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(TITLE)
 clock = pygame.time.Clock()
 
+
+# --- render image ---
+TILE_WALL_IMG = pygame.transform.scale(
+pygame.image.load('TileMap/tile_0000.png'),
+(TILE_SIZE, TILE_SIZE)
+)
+
+TILE_FLOOR_IMG = pygame.transform.scale(
+    pygame.image.load('TileMap/tile_0049.png'),
+    (TILE_SIZE, TILE_SIZE)
+)
+
+PLAYER_IMG = pygame.transform.scale(
+    pygame.image.load('Player animations/tile_0085.png'),
+    (TILE_SIZE, TILE_SIZE)
+)
+
 # --- Ready ---
-walls = make_map(tilemap_1)
-player = Player(100, 100, 'Player animations/tile_0085.png', 4)
+walls = make_map_colision(tilemap_1)
+floor = make_map_layers(tilemap_1)
+player = Player(100, 100, PLAYER_IMG, 4)
 
 # --- Bucle principal ---
 running = True
